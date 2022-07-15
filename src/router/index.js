626fe48f1d3b2d1ec45f7b57403730a05ccd136a
Hayeszy <1282429235@qq.com>
2022-07-15 17:54:00 +0800

    完成布局页面的路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,33 @@ const routes = [
   {
     path: '/login',
     component: () => import('@/views/Login')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/Layout'),
+    redirect: '/ ',
+    children: [
+      {
+        path: '/ ',
+        name: 'home',
+        component: () => import('@/views/Home')
+      },
+      {
+        path: '/video',
+        name: 'video',
+        component: () => import('@/views/Video')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/QA')
+      },
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/Profile')
+      }
+    ]
   }
 ]
 
